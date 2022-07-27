4a47ab093edcbded4e6f28320b7ba485f46ff64f
Joe Cross <dohbedoh@gmail.com>
2022-07-26 20:27:16 -0700

    Before Separating JavaScript

--- ParseJSON.js
+++ ParseJSON.js
@@ -2,16 +2,3 @@
 
 
 
-
-
-fetch("Homework.JSON")
-    .then (response => response.json())
-    .then(data => {
-        console.log( data )
-        //console.log("This is a console log.")
-        document.querySelector("#debug").innertext = data
-    })
-
-//let jsonData = JSON.parse(pufferfish);
-//console.log (jsondata.temperatureF[0]);
-
